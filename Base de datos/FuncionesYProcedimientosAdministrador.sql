e03ad58a4a3c49fa6e62cdd1ffa0722a17eede64
Yeray Campanario Fernández <thewalkerofdreams@gmail.com>
2019-11-19 10:41:34 +0100

    Update FuncionesYProcedimientosAdministrador.sql

--- Base de datos/FuncionesYProcedimientosAdministrador.sql
+++ Base de datos/FuncionesYProcedimientosAdministrador.sql
@@ -4,7 +4,7 @@
 Interfaz
 Nombre: addPartido
 Comentario: Este método nos permite insertar un nuevo partido en la base de datos.
-Cabecera: PROCEDURE addPartido(@resultadoLocal tinyint, @resultadoVisitante tinyint, @isAbierto bit, @MaxApuesta1 int, @MaxApuesta2 int, @MaxApuesta3 int, @FechaPartido smalldatetime, @IdCompeticion int, @Error smallint)
+Cabecera: PROCEDURE addPartido(@resultadoLocal tinyint, @resultadoVisitante tinyint, @isAbierto bit, @MaxApuesta1 int, @MaxApuesta2 int, @MaxApuesta3 int, @FechaPartido smalldatetime, @IdCompeticion int, @Error smallint OUTPUT)
 Entrada:
 	-@resultadoLocal tinyint
 	-@resultadoVisitante tinyint
@@ -20,7 +20,7 @@ Postcondiciones: El método devuelve un número entero asociado al nombre, -1 si m
 -2 si maxApuesta2 es igual o menor que cero o si es nulo, -3 si maxApuesta3 es igual o menor que cero o si es nulo, -4 si idCompeticion 
 es igual o menor que cero o si es nulo o 0 si se ha conseguido insertar el nuevo partido en la base de datos.
 */
-CREATE PROCEDURE addPartido(@resultadoLocal tinyint, @resultadoVisitante tinyint, @isAbierto bit, @MaxApuesta1 int, @MaxApuesta2 int, @MaxApuesta3 int, @FechaPartido smalldatetime, @IdCompeticion int, @Error smallint) AS
+CREATE PROCEDURE addPartido(@resultadoLocal tinyint, @resultadoVisitante tinyint, @isAbierto bit, @MaxApuesta1 int, @MaxApuesta2 int, @MaxApuesta3 int, @FechaPartido smalldatetime, @IdCompeticion int, @Error smallint OUTPUT) AS
 BEGIN
 	IF @MaxApuesta1 != null AND @MaxApuesta1 > 0
 	BEGIN
@@ -53,3 +53,26 @@ BEGIN
 		SET @Error = -1
 	END
 END
+
+GO
+
+/*
+Interfaz
+Nombre: abrirPartido
+Comentario: Este método nos permite abrir un patido de la base de datos.
+Cabecera: PROCEDURE abrirPartido(@IdPartido int, @FilasModificadas tinyint OUTPUT)
+Entrada:
+	-@IdPartido int
+Salida:
+	-@FilasModificadas
+Postcondiciones: Este método devuelve un número asociado al nombre, que son el número de filas modificadas.
+0 significa que no se ha encontrado el partido y 1 que se ha copnseguido modificar su estado.
+*/
+CREATE PROCEDURE abrirPartido(@IdPartido int, @FilasModificadas tinyint OUTPUT) AS
+BEGIN
+	UPDATE Partidos 
+		SET isAbierto = 0
+	WHERE id = @IdPartido
+	SET @FilasModificadas = @@ROWCOUNT	--Nos devuelve el número de filas afectadas en la transacción anterior
+END
+GO
