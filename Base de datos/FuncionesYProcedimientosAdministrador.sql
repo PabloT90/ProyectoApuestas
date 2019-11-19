e28d84552669ec009a6668799a4690e17c1c0ee8
Yeray Campanario Fernández <thewalkerofdreams@gmail.com>
2019-11-19 10:29:16 +0100

    Create FuncionesYProcedimientosAdministrador.sql

---
+++ Base de datos/FuncionesYProcedimientosAdministrador.sql
@@ -0,0 +1,55 @@
+--Tareas de administrador
+
+/*
+Interfaz
+Nombre: addPartido
+Comentario: Este método nos permite insertar un nuevo partido en la base de datos.
+Cabecera: PROCEDURE addPartido(@resultadoLocal tinyint, @resultadoVisitante tinyint, @isAbierto bit, @MaxApuesta1 int, @MaxApuesta2 int, @MaxApuesta3 int, @FechaPartido smalldatetime, @IdCompeticion int, @Error smallint)
+Entrada:
+	-@resultadoLocal tinyint
+	-@resultadoVisitante tinyint
+	-@isAbierto bit
+	-@MaxApuesta1 int
+	-@MaxApuesta2 int
+	-@MaxApuesta3 int
+	-@FechaPartido smalldatetime
+	-@IdCompeticion int
+Salida:
+	-@Error smallint
+Postcondiciones: El método devuelve un número entero asociado al nombre, -1 si maxApuesta1 es igual o menor que cero o si es nulo, 
+-2 si maxApuesta2 es igual o menor que cero o si es nulo, -3 si maxApuesta3 es igual o menor que cero o si es nulo, -4 si idCompeticion 
+es igual o menor que cero o si es nulo o 0 si se ha conseguido insertar el nuevo partido en la base de datos.
+*/
+CREATE PROCEDURE addPartido(@resultadoLocal tinyint, @resultadoVisitante tinyint, @isAbierto bit, @MaxApuesta1 int, @MaxApuesta2 int, @MaxApuesta3 int, @FechaPartido smalldatetime, @IdCompeticion int, @Error smallint) AS
+BEGIN
+	IF @MaxApuesta1 != null AND @MaxApuesta1 > 0
+	BEGIN
+		IF @MaxApuesta2 != null AND @MaxApuesta2 > 0
+		BEGIN
+			IF @MaxApuesta3 != null AND @MaxApuesta3 > 0
+			BEGIN
+				IF @IdCompeticion != null AND @IdCompeticion > 0
+				BEGIN
+					INSERT INTO Partidos VALUES(@resultadoLocal, @resultadoVisitante, @isAbierto, @MaxApuesta1, @MaxApuesta2, @MaxApuesta3, @FechaPartido, @IdCompeticion)
+					SET @Error = 0
+				END
+				ELSE
+				BEGIN
+					SET @Error = -4
+				END
+			END
+			ELSE
+			BEGIN
+				SET @Error = -3
+			END
+		END
+		ELSE
+		BEGIN
+			SET @Error = -2
+		END
+	END
+	ELSE
+	BEGIN
+		SET @Error = -1
+	END
+END
