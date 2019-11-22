8a03e77eafab4f815d91ae7416eb8b067d8f7323
Daniel Gordillo Rodriíguez <danielgordillo9@gmail.com>
2019-11-22 09:12:07 +0100

    Mira el comentario

--- Base de datos/InsertsSinUnique.sql
+++ Base de datos/InsertsSinUnique.sql
@@ -13,6 +13,8 @@ insert into Partidos values(4,5,1,50,30,35,'2019-3-12 11:00:00',(Select id From 
 insert into Partidos values(3,1,0,40,15,40,'2019-9-11 11:00:00',(Select id From Competiciones Where nombre = 'Copa del Rey'))
 insert into Partidos values(4,5,1,20,30,60,'2019-25-11 11:00:00',(Select id From Competiciones Where nombre = 'Copa autoestima'))
 insert into Partidos values(1,6,0,35,20,50,'2019-10-12 11:00:00',(Select id From Competiciones Where nombre = 'Copa autoestima'))
+insert into Partidos values(4,5,0,50,30,35,'2019-3-12 11:00:00',(Select id From Competiciones Where nombre = 'Copa del Rey'))
+
 
 SELECT * FROM Partidos
 insert into Partidos values(1,2,0,35,20,50,'2019-10-11 11:00:00',(Select id From Competiciones Where nombre = 'Copa del Rey'))
@@ -22,11 +24,13 @@ INSERT INTO Usuarios VALUES('sulviagurdilloabcdef@gmail.com','sul07',500)
 INSERT INTO Usuarios VALUES('decisionesdificile@gmail.com','dc19',1000)
 SELECT * FROM Usuarios
 
-Insert into Apuestas values(5.2,'2019-9-11 10:01:00',5.5,'sulviagurdilloabcdef@gmail.com',1,3,1)
+Insert into Apuestas values(5.2,'2019-9-11 10:01:00',5.5,'sulviagurdilloabcdef@gmail.com',19,3,1) --Pon aqui el ide de la apuesta que tenga en isAierta 0
 
-INSERT INTO Apuestas VALUES(2.8,'2019-10-11 10:00:00', 2.9,'decisionesdificile@gmail.com',2,2,1)
+INSERT INTO Apuestas VALUES(2.8,'2019-10-11 10:00:00', 2.9,'decisionesdificile@gmail.com',2,2,1) 
 
-INSERT INTO Apuestas VALUES(4.2,'2019-9-11 10:01:00',3.5,'sulviagurdilloabcdef@gmail.com',9,1,1)
+INSERT INTO Apuestas VALUES(4.2,'2019-9-11 10:01:00',3.5,'sulviagurdilloabcdef@gmail.com',9,1,1) 
+
+
 
 SELECT * FROM Apuestas
 
